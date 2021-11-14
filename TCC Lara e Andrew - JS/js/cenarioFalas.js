a2702b238ec7a6363ded588aa774f96ef3150d89
Lara Vian <lara.vsteinbach@gmail.com>
2021-11-14 14:27:20 -0300

    foi

---
+++ TCC Lara e Andrew - JS/js/cenarioFalas.js
@@ -0,0 +1,58 @@
+export {cenarioFalas}
+var cenarioFalas = {
+    "cenario": [
+    {
+        "comentario": "Primeiro cenario",
+            "falas": [
+                {
+                    "autor": "Khalisto",
+                    "frase": "Eu estou cansada daquela casa, meus pais não são ruins, mas as constantes brigas não me deixam mais ter uma boa noite de sono.",
+                    "posicao-balao": [0, 0]
+                },
+                {
+                    "autor": "Khalisto",
+                    "frase": "Minha mãe não aceita que eu não acredite no deus dela, não suporta que eu diga que não me sinto bem indo a igreja com ela, não gosta que eu me negue a rezar.",
+                    "posicao-balao": [0, 0]
+                },
+                {
+                    "autor": "Khalisto",
+                    "frase": "Ela me aceita enquanto pessoa trans, mas não aceita que eu não vá a igreja dela por não me sentir aceita lá.",
+                    "posicao-balao": [0, 0]
+                },
+                {
+                    "autor": "Khalisto",
+                    "frase": "Eu não aguento mais todo dia ter uma intriga diferente, preciso sair de casa, eu tenho como alugar uma casa só minha, vou fazer isso.",
+                    "posicao-balao": [0, 0]
+                },
+                {
+                    "autor": "Khalisto",
+                    "frase": "Preciso apenas ficar hoje fora de casa e depois planejar isso melhor",
+                    "posicao-balao": [0, 0]
+                }
+            ],
+        "opcoes": [
+            "Posso ir visitar minha amiga Amanda, ela é minha melhor amiga e acabou de se mudar",
+            "Posso ir visitar minha Avó Ingrid, ela tem o melhor bolo de cenoura do mundo"
+        ],
+        "numeroDialogos": 4,
+        "estados": [1,2],
+        "imagem": "../img/RUA.png",
+        "personagem": ""
+        
+    },{
+        "comentario": "cenario 1",
+                "falas": [
+                    {
+                        "autor": "Amanda",
+                        "frase": "O que ela te fez desta vez?",
+                        "posicao-balao": [0, 0]
+                    },
+                    {
+                        "autor": "Khalisto",
+                        "frase": "Apenas brigamos porque eu não quis rezar com ela, não foi feio, mas não aguento mais brigas todos os dias",
+                        "posicao-balao": [0, 0]
+                    }
+                ]}
+    ]
+}
+
