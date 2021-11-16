bca08fd9b870f31ba2e53f46bfe168e649ccc6c7
L4rhy <lara.vsteinbach@gmail.com>
2021-11-16 17:28:45 -0300

    atualizacoes dia 16/11

--- TCC Lara e Andrew - JS/js/jogo.js
+++ TCC Lara e Andrew - JS/js/jogo.js
@@ -2,38 +2,38 @@ var  cenarioFalas ={
     "cenario": [
     {
         "comentario": "Primeiro cenario",
-            "falas": [
-                {
-                    "autor": "Khalisto",
-                    "frase": "Eu estou cansada daquela casa, meus pais não são ruins, mas as constantes brigas não me deixam mais ter uma boa noite de sono.",
-                    "posicao-balao": [0, 0]
-                },
-                {
-                    "autor": "Khalisto",
-                    "frase": "Minha mãe não aceita que eu não acredite no deus dela, não suporta que eu diga que não me sinto bem indo a igreja com ela, não gosta que eu me negue a rezar.",
-                    "posicao-balao": [0, 0]
-                },
-                {
-                    "autor": "Khalisto",
-                    "frase": "Ela me aceita enquanto pessoa trans, mas não aceita que eu não vá a igreja dela por não me sentir aceita lá.",
-                    "posicao-balao": [0, 0]
-                },
-                {
-                    "autor": "Khalisto",
-                    "frase": "Eu não aguento mais todo dia ter uma intriga diferente, preciso sair de casa, eu tenho como alugar uma casa só minha, vou fazer isso.",
-                    "posicao-balao": [0, 0]
-                },
-                {
-                    "autor": "Khalisto",
-                    "frase": "Preciso apenas ficar hoje fora de casa e depois planejar isso melhor",
-                    "posicao-balao": [0, 0]
-                },
-                {
-                    "autor": "Khalisto",
-                    "frase": "Preciso apenas ficar hoje fora de casa e depois planejar isso melhor",
-                    "posicao-balao": [0, 0]
-                }
-            ],
+        "falas": [
+            {
+                "autor": "Khalisto",
+                "frase": "Eu estou cansada daquela casa, meus pais não são ruins, mas as constantes brigas não me deixam mais ter uma boa noite de sono.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Minha mãe não aceita que eu não acredite no deus dela, não suporta que eu diga que não me sinto bem indo a igreja com ela, não gosta que eu me negue a rezar.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Ela me aceita enquanto pessoa trans, mas não aceita que eu não vá a igreja dela por não me sentir aceita lá.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Eu não aguento mais todo dia ter uma intriga diferente, preciso sair de casa, eu tenho como alugar uma casa só minha, vou fazer isso.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Preciso apenas ficar hoje fora de casa e depois planejar isso melhor",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Preciso apenas ficar hoje fora de casa e depois planejar isso melhor",
+                "posicao-balao": [0, 0]
+            }
+        ],
         "opcoes": [
             "Posso ir visitar minha amiga Amanda, ela é minha melhor amiga e acabou de se mudar",
             "Posso ir visitar minha Avó Ingrid, ela tem o melhor bolo de cenoura do mundo"
@@ -47,17 +47,159 @@ var  cenarioFalas ={
         "comentario": "cenario 1",
         "falas": [
             {
-                        "autor": "Amanda",
-                        "frase": "O que ela te fez desta vez?",
-                        "posicao-balao": [0, 0]
-                    },
-                    {
-                        "autor": "Khalisto",
-                        "frase": "Apenas brigamos porque eu não quis rezar com ela, não foi feio, mas não aguento mais brigas todos os dias",
-                        "posicao-balao": [0, 0]
-                    }
-                ]}
-            ]
+                "autor": "Amanda",
+                "frase": "O que ela te fez desta vez?",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Apenas brigamos porque eu não quis rezar com ela, não foi feio, mas não aguento mais brigas todos os dias",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Amanda",
+                "frase": "Eu não sei como você aguenta isso há tanto tempo.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Ela me aceitou quando me assumi, sempre achei que era obrigação minha aceitar as loucuras dela.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Amanda",
+                "frase": "Não funciona assim, é obrigação dela te aceitar porque você é filha dela.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Eu sei, lá no fundo eu sei.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Amanda",
+                "frase": "Você não pode mais aceitar isso, só vai piorar se você não fizer nada.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Hoje eu decidi sair de casa.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Amanda",
+                "frase": "Você pode ficar aqui comigo.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Não, eu vou para casa e vou me planejar bem, eu tenho condições de morar sozinha, só preciso de um tempo para planejar tudo.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Amanda",
+                "frase": "Vem passar uns dias comigo e se planeja aqui, você ta mal com essas brigas, não quero que piore.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "eu não quero te dar trabalho.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Amanda",
+                "frase": "Não é trabalho, ou você vem morar comigo logo ou passar uns dias só até se mudar",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Eu realmente gosto das duas opções.",
+                "posicao-balao": [0, 0]
+            },
+        ],
+        "opcoes": [
+            "Posso vir morar com Amanda.",
+            "Posso passar alguns dias aqui, até me mudar para uma casa só minha."
+        ],
+        "numeroDialogos": 13,
+        "estados": [3,4],
+        "imagem": " ",
+        "personagem": ""
+    },{
+        "comentario": "cenario 2",
+        "falas": [
+            {
+                "autor": "Khalisto",
+                "frase": "Oi vó",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Ingrid",
+                "frase": "Olá Ká, por que tá com essa carinha?",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Eu e a minha mãe brigamos de novo.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Ingrid",
+                "frase": "Brigaram por quê?",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Os mesmo motivos bobos de sempre, vim aqui dar um tempo da gritaria lá de casa.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Ingrid",
+                "frase": "Você devia sair daquela casa, você já tem como se manter.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Na verdade eu estava pensando nisso, vou sair de casa, só preciso saber para onde ainda.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Ingrid",
+                "frase": "Vem morar comigo.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Não acho que seja uma boa ideia vó",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Ingrid",
+                "frase": "Eu acho, preciso de alguém para lavar a louça enquanto vejo minha novela.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Khalisto",
+                "frase": "Até que seria bom, mas não quero dar trabalho.",
+                "posicao-balao": [0, 0]
+            },
+            {
+                "autor": "Ingrid",
+                "frase": "Você não dá trabalho fica aqui comigo, nem que seja por alguns dias até achar outro lugar.",
+                "posicao-balao": [0, 0]
+            }
+        ],
+        "opçoes": [
+            "Posso vir morar com minha Avó.",
+            "posso passar alguns dias com ela."
+        ],
+        "numeroDialogos": 11,
+        "estados": [5,6],
+        "imagem": "img/casaAvo.png",
+        "personagem": ""
+    }
+    
+]
 }
 var i = 0
 var estadoAtual = 0
